9738214902fc5eb89e45c1f219c69bfab882bea0
MoonHyekyung <moonsoft02@gmail.com>
2019-12-01 13:37:32 +0900

    Product 헤더입니다.

---
+++ 편매점 프로젝트/Product.h
@@ -0,0 +1,16 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+#include "intro.h"
+using namespace std;
+struct Product {
+public:
+	int number; // 상품번호
+	string name; // 상품명
+	int price; // 가격
+	string manufacturer; // 제조사
+	string taste; // 맛
+	int cal; // 열량
+}typedef Pro;
+
+#endif // !PRODUCT_H
+
