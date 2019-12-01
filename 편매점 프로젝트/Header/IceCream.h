d027a97a3a32fdbcce909027f05e70ac8b968b48
JINNNNS <baekhuen@naver.com>
2019-12-01 21:44:16 +0900

    바뀐 icecream

--- 편매점 프로젝트/Header/IceCream.h
+++ 편매점 프로젝트/Header/IceCream.h
@@ -1,11 +1,47 @@
 #ifndef ICECREAM_H
 #define ICECREAM_H
-#include "Product.h"
+#include "intro.h"
+#include "Category.h"
 
-class IceCream :public Product {
-public:
-	IceCream(int number, string name, int price, string manufacturer, string taste, int cal)
-		:Product(number, name, price, manufacturer, taste, cal) { }
-};
+
+#include <string.h>
+
+#include <stdio.h>
+
+#include <mysql.h>
+
+
+
+#pragma comment(lib, "libmysql.lib")
+
+#pragma comment(lib, "ws2_32.lib")  
+
+
+
+#define DB_HOST "10.96.122.63"//"호스트IP, 도메인또는localhost"
+
+#define DB_USER "root"
+
+#define DB_PASS "mirim2"
+
+#define DB_NAME "PyeonMarket"
+
+
+
+
+
+#define SQL_SELECT_RECORD3 "SELECT * FROM icecream"
+MYSQL *connection3 = NULL, conn3;
+
+MYSQL_RES *sql_result3;
+
+MYSQL_ROW sql_row3;
+
+int query_stat3;
+
+
+
+
+
 
 #endif
