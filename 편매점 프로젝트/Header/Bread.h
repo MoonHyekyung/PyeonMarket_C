9a368a84c602ccfd9c6fb209d922e89db69ed1e2
JINNNNS <baekhuen@naver.com>
2019-12-01 21:43:57 +0900

    바뀐 DB Bread

--- 편매점 프로젝트/Header/Bread.h
+++ 편매점 프로젝트/Header/Bread.h
@@ -1,11 +1,57 @@
 #ifndef BREAD_H
 #define BREAD_H
-#include "Product.h"
+#include "intro.h"
+#include "Category.h"
 
-class Bread :public Product {
+
+#include <string.h>
+
+#include <stdio.h>
+
+#include <mysql.h>
+
+
+
+#pragma comment(lib, "libmysql.lib")
+
+#pragma comment(lib, "ws2_32.lib")  
+
+
+
+#define DB_HOST "10.96.122.63"//"호스트IP, 도메인또는localhost"
+
+#define DB_USER "root"
+
+#define DB_PASS "mirim2"
+
+#define DB_NAME "PyeonMarket"
+
+
+
+#define SQL_SELECT_RECORD "SELECT * FROM bread"
+
+MYSQL *connection = NULL, conn;
+
+MYSQL_RES *sql_result;
+
+MYSQL_ROW sql_row;
+
+int query_stat;
+
+int i;
+/*
+
+class bread{
 public:
-	Bread(int number, string name, int price, string manufacturer, string taste, int cal)
-		:Product(number, name, price, manufacturer, taste, cal){ }
+	bread() {
+		Rectangle(100, 100);
+	}
+	void Rectangle(int width, int height);
 };
+
+
+void Rectangle(int width, int height);
+*/
+
 
 #endif
